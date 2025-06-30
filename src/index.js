1e94cb1404e323c51f87bbddb6eabcc2954d9e74
Aman Gill <agill@sidatasolutions.com>
2025-06-30 10:43:06 -0600

    Added a page without styling

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ import {
   Register,
   Checkout,
   PageNotFound,
+  NoStyling
 } from "./pages";
 import ScrollToTop from "./components/ScrollToTop";
 import { Toaster } from "react-hot-toast";
@@ -38,6 +39,7 @@ root.render(
           <Route path="/checkout" element={<Checkout />} />
           <Route path="*" element={<PageNotFound />} />
           <Route path="/product/*" element={<PageNotFound />} />
+          <Route path="/no-styling" element={<NoStyling />} />
         </Routes>
       </Provider>
     </ScrollToTop>
